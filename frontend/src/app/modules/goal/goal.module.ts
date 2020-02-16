4d51ce87202e5a69094e8fa0bae5e4bdffc11fba
Fotso <a_loic16@yahoo.fr>
2020-02-16 12:27:32 +0100

    Designed project angular structure

---
+++ frontend/src/app/modules/goal/goal.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { GoalComponent } from './goal.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [GoalComponent]
+})
+export class GoalModule { }
